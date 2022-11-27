6497a8185ae6fd1b5204c567fcde37e7b0a4e21f
Animan03 <102158944+Animan03@users.noreply.github.com>
2022-11-27 18:25:02 +0530

    Update Login.js

--- src/client/src/Login.js
+++ src/client/src/Login.js
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-import './cssfiles/signin.css';
+import './cssfiles/App.css';
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import SignUp from "./SignUp";
